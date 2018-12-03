ddcb7cde6a7af64869c15f3f9712e50530738846
Puritz <pop-purit@hotmail.com>
2018-12-03 17:31:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
 echo "Hello LINE BOT";
-$old=21;
-session_register ( "old" );
-if($_SESSION["old"]>0){
-			$_SESSION["old"]= $_SESSION["old"] + 1;
-			}
-			else{	
-			$_SESSION["old"]=2;
-			}
-echo $_SESSION["old"];
+$user = "bamboo";
+session_register ( "user" );
+echo $_SESSION["user"];
+
