5633b56a32abc842df1bc3dd51a32a15a9149751
Puritz <pop-purit@hotmail.com>
2018-12-03 17:30:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 echo "Hello LINE BOT";
 $old=21;
- Session_register("old")
-if($_SESSION['old']>0){
-			$_SESSION['old']= $_SESSION['old'] + 1;
+session_register ( "old" );
+if($_SESSION["old"]>0){
+			$_SESSION["old"]= $_SESSION["old"] + 1;
 			}
 			else{	
-			$_SESSION['old']=2;
+			$_SESSION["old"]=2;
 			}
-echo $_SESSION['old'];
+echo $_SESSION["old"];
