54c75b40e37092bd117dabf801d4875853072e7b
Puritz <pop-purit@hotmail.com>
2018-12-03 20:51:35 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -209,7 +209,7 @@ use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuild
 			$multiMessage = new MultiMessageBuilder;
 			$multiMessage->add($textMessage); 
 			$replyData = $multiMessage;
-			$response = $bot->pushMessage($ID,$relyData);
+			
 			}
 			
 			
@@ -220,7 +220,7 @@ use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuild
 			$multiMessage = new MultiMessageBuilder;
 			$multiMessage->add($textMessage); 
 			$replyData = $multiMessage;
-			$response = $bot->pushMessage($ID,$relyData);
+			
 			}
 			
 
@@ -231,9 +231,9 @@ use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuild
 			$multiMessage = new MultiMessageBuilder;
 			$multiMessage->add($textMessage); 
 			$replyData = $multiMessage;
-			$response = $bot->pushMessage($ID,$relyData);
+			
 			}
-				
+				$response = $bot->pushMessage($id,$replyData);
 			}
 			
 			
