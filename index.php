e5378a032ad0ff4a453b5e2135107095d58962ae
Puritz <pop-purit@hotmail.com>
2018-12-03 17:34:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 echo "Hello LINE BOT";
-session_start ();
-$user = "bamboo";
-session_register ( "user" );
-echo $_SESSION["user"];
+Session_Start();
+$name="นายวีระชัย นุกิจรัมย์";
+ $old=21;
+ Session_register("name");
+ Session_register("old")
+   echo "ค่าใน Session Name คือ $name <br>";
+echo "ค่าใน Session Old คือ $old <br>";
 ?>
