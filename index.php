7a99f870182118281dd73c61894bcfb7285c8d20
Puritz <pop-purit@hotmail.com>
2018-12-03 17:38:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
 <?php
-
-session_start();
-$name="นายวีระชัย นุกิจรัมย์";
- $old=21;
- session_register("name");
- session_register("old");
-   echo "ค่าใน Session Name คือ $name <br>";
-echo "ค่าใน Session Old คือ $old <br>";
 echo "Hello LINE BOT";
 ?>
