ce9e4d5e8bce6fccc9c74f9e07787d9e6f03f975
Puritz <pop-purit@hotmail.com>
2018-12-06 11:36:42 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -233,18 +233,6 @@ use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuild
 				$tmp[0] = $tmp[0] + 1;
 				$tmp[1] = $tmp[1] . $message . ",";
 				
-				if($tmp[0] == 0){
-				$question = "คุณคิดว่าคุณสามารถทำได้ดีกว่านี้"
-				}
-				
-				if($tmp[0] == 1){
-				$question = "หฟกด"
-				}
-				
-				if($tmp[0] == 2){
-				$question = "123"
-				}
-				
 				
 				$myfile = fopen($fileName, "w");
 				$txtW = $tmp[0] . "|" . $tmp[1];
@@ -264,7 +252,7 @@ use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuild
 			$textReplyMessage = "next question ok " . $tmp[0];
                 	$textMessage = new TextMessageBuilder($textReplyMessage); 
 			
-			$textReplyMessage2 = $question;
+			$textReplyMessage2 = "123";
                 	$textMessage2 = new TextMessageBuilder($textReplyMessage2); 
 			
 			$multiMessage = new MultiMessageBuilder;
