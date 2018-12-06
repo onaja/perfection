f9fdcd2b1911c23555a1c5d6e3bfb2a399281b62
Puritz <pop-purit@hotmail.com>
2018-12-06 14:03:09 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -222,7 +222,7 @@ use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuild
 				fwrite($myfile, $txtW);
 				fclose($myfile);
 				
-				$tmp[1] = $tmp[1] . $message . ",";
+				$answer = $tmp[1] . $message . ",";
 			} else {
 				
 				$myfile = fopen($fileName, "r");
